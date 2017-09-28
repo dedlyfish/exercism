4febdad46b7ae60e21a680b218b39d61a90e5ef4
Andrey <fish@dedlyfish.com>
2017-09-29 10:32:17 +1100

    Better algorithm for anagram

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,14 +1,6 @@
 def detect_anagrams(word, array):
     res = []
     for i in array:
-            is_anagram = True
-            for j in i:
-                    if (len(word) != len(i)):
-                        is_anagram = False
-                        break
-                    if (word.lower().count(j) != i.lower().count(j)) or (word.lower() == i.lower()):
-                        is_anagram = False
-                        break
-            if is_anagram:
-                res.append(i)
+        if (sorted(word.lower()) == sorted(i.lower())) and (word.lower() != i.lower()):
+            res.append(i)
     return res
