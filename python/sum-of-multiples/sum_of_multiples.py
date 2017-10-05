971e453622592c53f135d7b5deed3653a2ea0215
Andrey <fish@dedlyfish.com>
2017-10-06 10:13:39 +1100

    Optimized sum of multiples

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,6 +1,6 @@
 def sum_of_multiples(num, multiples):
     r = []
     for i in range(1, num):
-        if sum(list(map(lambda x: 1 if i % x else 0, multiples))) <= len(multiples) - 1:
+        if not all(list(map(lambda x: True if i % x else False, multiples))):
             r.append(i)
     return sum(r)
